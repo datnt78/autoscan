2e25f1d898e0c2d00efe670e15a6e7133d1597cb
ping-0day <zjandzj11@163.com>
2022-02-28 22:55:33 +0800

    1

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd /root
 wget https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/706915/chrome-linux.zip
 unzip chrome-linux.zip && sleep 5 && mv chrome-linux /root/chrome &&rm -f chrome-linux.zip && chmod +x /root/chrome/chrome
 #下载cent
-wget https://github.com/xm1k3/cent/releases/download/v1.0/cent && chmod +x cent && move cent /usr/bin/
+wget https://github.com/xm1k3/cent/releases/download/v1.0/cent && chmod +x cent && mv /root/cent /usr/bin/
 #下载githua
 wget https://github.com/ping-0day/autoscan/archive/refs/heads/main.zip
 unzip main.zip
